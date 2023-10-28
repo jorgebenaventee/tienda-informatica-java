11dc52a0f40c06dcec107e39e32255ff0a3af87f
jaimemen897 <jaimemen897@gmail.com>
2023-10-28 13:36:13 +0200

    Initial commit

---
+++ src/main/java/dev/clownsinformatics/tiendajava/TiendaJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.clownsinformatics.tiendajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiendaJavaApplication.class, args);
+    }
+
+}
