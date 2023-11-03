7f2f5c3401809a2360cef46a984f0633e5732f38
jaimemen897 <jaimemen897@gmail.com>
2023-11-03 16:51:31 +0100

    Se crea CategoryService y se implementan todos los métodos

--- src/main/java/dev/clownsinformatics/tiendajava/categories/repositories/CategoryRepository.java
+++ src/main/java/dev/clownsinformatics/tiendajava/categories/repositories/CategoryRepository.java
@@ -7,13 +7,18 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<List<Category>> findAllByNameContainingIgnoreCase(String name);
 
+    Optional<Category> findByUuid(UUID uuid);
+
+    Optional<Category> findByName(String name);
+
     @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Product p WHERE p.category.uuid = :id")
-    Boolean existsFunkoById(Long id);
+    Boolean existsProductById(UUID id);
 
     @Query("SELECT p.uuid FROM Category p WHERE p.name = :name")
     Optional<Long> getIdByName(String name);
