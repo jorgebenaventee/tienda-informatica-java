efd6689181b5439473bb78171450fa708f991370
jaimemen897 <jaimemen897@gmail.com>
2023-11-03 17:29:46 +0100

    Se crea CategoryController y implementan todos sus métodos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,11 @@
-INSERT INTO CATEGORIES (name)
-VALUES ('Categoría A');
+INSERT INTO categories (uuid, name)
+VALUES ('d69cf3db-b77d-4181-b3cd-5ca8107fb6a9', 'DEPORTES'),
+       ('6dbcbf5e-8e1c-47cc-8578-7b0a33ebc154', 'COMIDA'),
+       ('9def16db-362b-44c4-9fc9-77117758b5b0', 'BEBIDA'),
+       ('8c5c06ba-49d6-46b6-85cc-8246c0f362bc', 'COMPLEMENTOS'),
+       ('bb51d00d-13fb-4b09-acc9-948185636f79', 'OTROS');
 
 INSERT INTO PRODUCTS (id, NAME, WEIGHT, PRICE, IMG, STOCK, DESCRIPTION, CATEGORY_ID)
-VALUES (random_uuid(), 'Producto A', 1, 100, 'hola jaja img', 10, 'Descripción del producto A', random_uuid());
+VALUES ('d69cf3db-b77d-4181-b3cd-5ca8107fb6a0', 'Producto A', 1, 100, 'hola jaja img', 10, 'Descripción del producto A', '9def16db-362b-44c4-9fc9-77117758b5b0');
 
 
