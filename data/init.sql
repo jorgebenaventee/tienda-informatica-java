a786ced0310d5cc52b4ab79a6615d89cf241a3e2
David Jaraba <velcas669@gmail.com>
2023-12-02 12:50:34 +0100

    Fix docker db

--- data/init.sql
+++ data/init.sql
@@ -9,13 +9,10 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
-DROP DATABASE IF EXISTS "tienda-informatica";
 --
 -- Name: tienda-informatica; Type: DATABASE; Schema: -; Owner: postgres
 --
 
-CREATE DATABASE "tienda-informatica" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'en_US.utf8';
 
 
 ALTER DATABASE "tienda-informatica" OWNER TO postgres;
